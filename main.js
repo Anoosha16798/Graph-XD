764220950f5760ab76bac039bf4f6f5fb95a14e2
Amith Annigeri <amithannigeri@Amiths-MacBook-Air.local>
2020-04-07 14:00:25 +0530

    added clear button

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
-
-  const { localFileSystem:fs, formats } = require("uxp").storage;
+const { localFileSystem: fs, formats } = require("uxp").storage;
 
 const MODES = {
   URL: "urlPane",
-  FILE: "filePane"
+  FILE: "filePane",
 };
 
 function panel() {
   let rootNode = null;
-  let mode = MODES.FILE;       // which mode -- adding image by URL or File?
-  let thingsToDrag = [];      // list of things to drag
+  let mode = MODES.FILE; // which mode -- adding image by URL or File?
+  let thingsToDrag = []; // list of things to drag
 
   // convenience! We're going to be typing this a lot.
-  const $ = sel => rootNode && rootNode.querySelector(sel);
+  const $ = (sel) => rootNode && rootNode.querySelector(sel);
 
   const PANEL_HTML = `
 <style>
-.droptarget {
-  float: left; 
-  width: 100px; 
-  height: 35px;
-  margin: 15px;
-  margin-right: 100px;
-  padding: 10px;
-  border: 1px solid #aaaaaa;
-}
+
   .pane {
     border: 1px solid #808080;
     border-radius: 8px;
@@ -97,7 +88,7 @@ function panel() {
   // Make panes visible based upon the user's current action
   function updatePaneVisibility() {
     Object.entries(MODES).forEach(([k, v]) => {
-      $(`#${v}`).style.display = (mode === v) ? "initial" : "none";
+      $(`#${v}`).style.display = mode === v ? "initial" : "none";
     });
   }
 
@@ -105,9 +96,7 @@ function panel() {
   function addImageToDrag(file, url) {
     // this is the _visual_ representation of what the user will drag
     const img = document.createElement("img");
-    
 
-    
     img.src = url;
     img.height = 100;
     $("#divThingsToDrag").appendChild(img);
@@ -116,13 +105,14 @@ function panel() {
     // base64 data, use data:/image/type;base64,....
     thingsToDrag.push(file.nativePath);
   }
- 
-  
+
   /*
    * Create the panel's DOM. We'll do this only once.
    */
   function create() {
-    if (rootNode) { return rootNode; }
+    if (rootNode) {
+      return rootNode;
+    }
 
     rootNode = document.createElement("div");
     rootNode.innerHTML = PANEL_HTML;
@@ -134,30 +124,20 @@ function panel() {
     $("#lstAction").onchange = (evt) => {
       mode = evt.target.value;
       updatePaneVisibility();
-    }
+    };
 
     // The user is starting to drag our image grid!
-    $("#divThingsToDrag").addEventListener("dragstart", evt => {
+    $("#divThingsToDrag").addEventListener("dragstart", (evt) => {
       if (thingsToDrag.length > 0) {
         // show the copy drag and drop effect (various by OS)
         evt.dataTransfer.dropEffect = "move";
         // and add what we'll copy -- in this case a list of file paths (or data URIs)
-       evt.dataTransfer.setData("text/uri-list", thingsToDrag.join("\n")); 
-
-
-       var dataList = evt.dataTransfer.items;
-       dragLeave(dataList);
+        evt.dataTransfer.setData("text/uri-list", thingsToDrag.join("\n"));
       }
     });
-    function dragLeave(event) {
-      if ( event.target.className == "droptarget" ) {
-        document.getElementById("demo").innerHTML = "Left the dropzone";
-        event.target.style.border = "";
-      }
-    }
-    
+
     $("#btnDownloadImage").onclick = async () => {
-      const url = 'assets/AreaChart.svg';
+      const url = "assets/AreaChart.svg";
       const r = await fetch(url, { "Content-Type": "image/svg" });
       const b = await r.arrayBuffer();
       const imgArray = new Uint8Array(b);
@@ -168,116 +148,127 @@ function panel() {
       // polyfill, and this is shorter (and faster)
       // Caveat: we don't clean up after ourselves. You should.
       const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-     
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+
       await imageFile.write(imgArray, { format: formats.binary });
       addImageToDrag(imageFile, url);
-    }
+    };
     $("#btnLoadImage").onclick = async () => {
-      const imageFile = await fs.getFileForOpening({types: ["jpg", "png", "svg", "gif"]});
+      const imageFile = await fs.getFileForOpening({
+        types: ["jpg", "png", "svg", "gif"],
+      });
       if (imageFile) {
         addImageToDrag(imageFile, imageFile.url);
       }
-    }
+    };
 
-//Clear images -------------------------------------------------------------------------------------------------------------------------
+    //Clear images -------------------------------------------------------------------------------------------------------------------------
 
-
-$("#clearImages").onclick=async()=>{
- // document.getElementById("img");
-// $('#divThingsToDrag').clear();
-for (var i = 0; i < thingsToDrag.length; i++) {
-  $('#divThingsToDrag').remove(i);
-}
-}
-
-//Add Images----------------------------------------------------------------------------------------------------------
-
-$('#addImages').onclick=async()=>{
-  
-}
-
-
-
-//AreaChart-------------------------------------------------------------------------------
-  $("#AreaChart").onclick=async()=>{
-      const url = 'assets/AreaChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-    
-}
-   //LineChart-------------------------------------------------------------------------------
-    $("#LineChart").onclick = async () => {
-      const url = 'assets/LineChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-      
-     }
-
-     //BarChart-------------------------------------------------------------------------------
-     $("#BarChart").onclick = async () => {
-      const url = 'assets/BarChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-      
-     }
-
-
-//PieChart-------------------------------------------------------------------------------
-     $("#PieChart").onclick = async () => {
-      const url = 'assets/PieChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-      
-     }
-
-     //ScatteredChart-------------------------------------------------------------------------------
-     $("#ScatteredChart").onclick = async () => {
-      const url = 'assets/ScatteredChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-      
-     }
-
-     //TwoLevelPieChart-------------------------------------------------------------------------------
-     $("#TwoLevelPieChart").onclick = async () => {
-      const url = 'assets/TwoLevelPieChartE.svg'
-      const r = await fetch(url, { "Content-Type": "image/svg" });
-      const b = await r.arrayBuffer();
-      const imgArray = new Uint8Array(b);
-      const tempFolder = await fs.getTemporaryFolder();
-      const imageFile = await tempFolder.createFile(`temp${thingsToDrag.length}.svg`, { overwrite: true });
-      await imageFile.write(imgArray, { format: formats.binary });
-      addImageToDrag(imageFile, url);
-      
-     }
-        return rootNode;
+    $("#clearImages").onclick = async () => {
+      // document.getElementById("img");
+      // $('#divThingsToDrag').clear();
+      for (var i = 0; i < thingsToDrag.length; i++) {
+        $("#divThingsToDrag").remove(i);
       }
+    };
+
+    //Add Images----------------------------------------------------------------------------------------------------------
+
+    $("#addImages").onclick = async () => {};
+
+    //AreaChart-------------------------------------------------------------------------------
+    $("#AreaChart").onclick = async () => {
+      const url = "assets/AreaChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+    //LineChart-------------------------------------------------------------------------------
+    $("#LineChart").onclick = async () => {
+      const url = "assets/LineChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+
+    //BarChart-------------------------------------------------------------------------------
+    $("#BarChart").onclick = async () => {
+      const url = "assets/BarChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+
+    //PieChart-------------------------------------------------------------------------------
+    $("#PieChart").onclick = async () => {
+      const url = "assets/PieChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+
+    //ScatteredChart-------------------------------------------------------------------------------
+    $("#ScatteredChart").onclick = async () => {
+      const url = "assets/ScatteredChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+
+    //TwoLevelPieChart-------------------------------------------------------------------------------
+    $("#TwoLevelPieChart").onclick = async () => {
+      const url = "assets/TwoLevelPieChartE.svg";
+      const r = await fetch(url, { "Content-Type": "image/svg" });
+      const b = await r.arrayBuffer();
+      const imgArray = new Uint8Array(b);
+      const tempFolder = await fs.getTemporaryFolder();
+      const imageFile = await tempFolder.createFile(
+        `temp${thingsToDrag.length}.svg`,
+        { overwrite: true }
+      );
+      await imageFile.write(imgArray, { format: formats.binary });
+      addImageToDrag(imageFile, url);
+    };
+    return rootNode;
+  }
 
   /*
    * Attach the panel's UI to event.node, which is the UI's mount point provided by XD.
@@ -287,13 +278,12 @@ $('#addImages').onclick=async()=>{
   }
 
   return {
-    show
-  }
+    show,
+  };
 }
 
 module.exports = {
-    panels: {
-        dnd: panel()
-    }
+  panels: {
+    dnd: panel(),
+  },
 };
-
